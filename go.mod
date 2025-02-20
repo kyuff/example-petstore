9707a9fb13bb28854eb667e19469e31315ddf519
Søren Kyndi-Wiuff <soren@sxl.dk>
2025-02-20 10:47:57 +0100

    Add command validation (#3)

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.24.0
 require (
 	github.com/kyuff/anchor v0.0.0-20250218074811-b53c5dd66dca
 	github.com/kyuff/es v0.0.0-20250218102440-e65fa431eec9
-	github.com/kyuff/es-commands v0.0.0-20250219093043-30ae79686c3c
+	github.com/kyuff/es-commands v0.0.0-20250220094239-c871507a577a
+	github.com/kyuff/validate v0.0.0-20250220092053-10750838ba8d
 	github.com/oapi-codegen/runtime v1.1.1
 )
 
--- internal/app/dispatcher.go
+++ internal/app/dispatcher.go
@@ -4,14 +4,16 @@ import (
 	"errors"
 
 	"github.com/kyuff/anchor"
-	"github.com/kyuff/es-commands"
+	commands "github.com/kyuff/es-commands"
 	"github.com/kyuff/example-petstore/internal/domain/pets"
+	"github.com/kyuff/validate"
 )
 
 var dispatcher = anchor.Singleton(func() (*commands.Dispatcher, error) {
 	return commands.NewDispatcher(
 		eventStore(),
 		commands.SLogMiddleware(logger()),
+		commands.Validate(validate.Middleware),
 	), nil
 })
 
